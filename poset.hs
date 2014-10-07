a40b0905c9781e8779847d6947888d1177138756
Tomasz Tylec <ttylec@gmail.com>
2014-10-07 11:43:56 +0200

    Few more structure testing functions.
    Optimization of greatest lower bound and lower upper bound.

--- poset.hs
+++ poset.hs
@@ -3,6 +3,7 @@ module Poset where
 
 import Control.Monad
 import Data.List
+import Data.Maybe
 
 -- |Type class for finite element collection
 class Finite a where
@@ -68,6 +69,7 @@ newtype Space4 = Space4 [Int] deriving (Show)
 
 instance Eq Space4 where
         (Space4 a) == (Space4 b) = (sort a) == (sort b)
+
 instance Finite Space4 where
         elements = map Space4 $ subsets [1, 2, 3, 4] 
 
@@ -81,7 +83,9 @@ instance Logic Space4 where
         (Space4 a) /\ (Space4 b) = Just $ Space4 $ intersect a b
         (Space4 a) \/ (Space4 b) = Just $ Space4 $ union a b
         
-
+--
+-- Creating new 
+--
 data Product a b = Product a b deriving (Eq, Show)
 
 instance (Finite a, Finite b) => Finite (Product a b) where
@@ -141,48 +145,64 @@ lessThan :: (Logic a) => a -> [a]
 lessThan p = filter (.<. p) $ elements
 
 lowestUpperBound :: (Logic a) => a -> a -> [a]
-lowestUpperBound p q = minimal $ intersect (greaterThan p) (greaterThan q)
+lowestUpperBound p q = minimal $ filter (\r -> (p .<. r) && (q .<. r)) $ elements
 
 greatestLowerBound :: (Logic a) => a -> a -> [a]
-greatestLowerBound p q = maximal $ intersect (lessThan p) (lessThan q)
+greatestLowerBound p q = maximal $ filter (\r -> (r .<. p) && (r .<. q)) $ elements
 
--- (\/) :: (Logic a) => a -> a -> Maybe a
--- p \/ q
---     | length lub == 1 = Just $ head lub
---     | otherwise = Nothing
---     where
---         lub = lowestUpperBound p q
--- 
--- (/\) :: (Logic a) => a -> a -> Maybe a
--- p /\ q
---     | length glb == 1 = Just $ head glb
---     | otherwise = Nothing
---     where
---         glb = greatestLowerBound p q
+coversOf :: (Logic a) => a -> [a]
+coversOf a = minimal $filter (not . (a ==)) $ greaterThan a
 
 hasJoin :: (Logic a) => a -> a -> Bool
-hasJoin p q = (p \/ q) /= Nothing
+hasJoin p q = isJust $ p \/ q
  
 checkOrderReverse :: (Logic a) => [a] -> Bool
 checkOrderReverse ps = all id [(ortho q) .<. (ortho p) | 
                               p <- ps, q <- ps, p .<. q]
 
-(-|-) :: (Logic a) => a -> a -> Bool
-p -|- q = p .<. ortho q
+isOrthogonal :: (Logic a) => a -> a -> Bool
+isOrthogonal p q = p .<. ortho q
+
+isCompatible :: (Logic a) => a -> a -> Bool
+isCompatible a b = all (id) $ map (fromMaybe False) [are_ortho, are_equal_a, are_equal_b]
+    where 
+          are_equal_a = liftM2 (==) aa $ Just a
+          are_equal_b = liftM2 (==) bb $ Just b
+          are_ortho = liftM mutuallyOrthogonal $ sequence [a1, b1, c] 
+          c = a /\ b
+          a1 = a /\ ortho b
+          b1 = b /\ ortho a
+          aa = join $ liftM2 (\/) a1 c
+          bb = join $ liftM2 (\/) b1 c
+
+mutuallyOrthogonal :: (Logic a) => [a] -> Bool
+mutuallyOrthogonal = mutuallyBy isOrthogonal
+
+mutuallyCompatible :: (Logic a) => [a] -> Bool
+mutuallyCompatible = mutuallyBy isCompatible
+
+mutuallyBy :: (a -> a -> Bool) -> [a] -> Bool
+mutuallyBy _ [] = True
+mutuallyBy f (a:as) = (all (f a) as) && mutuallyBy f as
+
+-- isRegular :: (Logic a) => [a] -> Bool
+-- isRegular [] = True
+-- isRegular (a:as) = 
 
 checkSupremum :: (Logic a) => [a] -> Bool
 checkSupremum ps = all ((/= Nothing) . orthosup) ps
     where
-        orthosup p = foldM (\/) p [q | q <- ps, p -|- q]
+        orthosup p = foldM (\/) p [q | q <- ps, p `isOrthogonal` q]
 
 checkOrthomodular :: (Logic a) => [a] -> Bool
 checkOrthomodular ps = all id [(Just b) == (rhs a b) | a <- ps, b <- ps, a .<. b]
     where
-        rhs a b = (b /\ ortho a) >>= (a \/) 
+        rhs a b = (b /\ ortho a) >>= (\/ a) 
 
 debugOrthomodular ps = filter (\ (x, y, z) -> Just y /= z) $ [(a, b, (rhs a b)) | a <- ps, b <- ps, a .<. b]
     where
         rhs a b = (b /\ ortho a) >>= (a \/) 
+
 --
 -- Poset functions
 --
@@ -207,9 +227,3 @@ maximal (p:ps)
 subsets :: [Int] -> [[Int]]
 subsets []  = [[]]
 subsets (x:xs) = subsets xs ++ map (x:) (subsets xs)
-
--- union :: (Eq a) => [a] -> [a] -> [a]
--- union ps qs = sort $ ps ++ (qs `setminus` ps)
--- 
--- setminus :: (Eq a) => [a] -> [a] -> [a]
--- a `setminus` b = sort $ filter (not . (`elem` b)) a
