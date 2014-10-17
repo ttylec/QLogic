55411341bf09caa60c13a97da036ace247cb9906
Tomasz Tylec <ttylec@gmail.com>
2014-10-17 14:02:04 +0200

    BoxProduct optimizations

--- Makefile
+++ Makefile
@@ -1,9 +1,21 @@
-GHCOPTS=-O3 -threaded -rtsopts -XBangPatterns -fforce-recomp -auto-all -caf-all -prof
+GHCOPTS=-O3 -threaded -rtsopts -XBangPatterns -fforce-recomp
+GHCDEBUG= -auto-all -caf-all -prof
 
 all: effi bwo
 
-effi: effi.hs qlogic.hs examples.hs
+debug: effi-debug bwo-debug
+
+effi: effi.hs qlogic.hs examples.hs QLogic/boxproduct.hs QLogic/boxworld.hs
 	ghc $(GHCOPTS) -o effi effi.hs
 
 bwo: bwo.hs qlogic.hs QLogic/boxproduct.hs QLogic/boxworld.hs
 	ghc $(GHCOPTS) -o bwo bwo.hs
+
+effi-debug: effi.hs qlogic.hs examples.hs QLogic/boxproduct.hs QLogic/boxworld.hs
+	ghc $(GHCOPTS) $(GHCDEBUG) -o effi effi.hs
+
+bwo-debug: bwo.hs qlogic.hs QLogic/boxproduct.hs QLogic/boxworld.hs
+	ghc $(GHCOPTS) $(GHCDEBUG) -o bwo bwo.hs
+
+clean:
+	rm bwo effi
--- QLogic/boxproduct.hs
+++ QLogic/boxproduct.hs
@@ -5,13 +5,21 @@ import Data.List
 import Data.Maybe
 import Data.Function
 import Control.Monad
-import QLogic.BoxWorld
+import Data.Typeable
 
-data FreeProduct a b = FreeProd a b | FreePlus (FreeProduct a b) (FreeProduct a b) --deriving (Show)
+class Repr a where
+        repr :: a -> String
+
+data FreeProduct a b = FreeProd a b | FreePlus (FreeProduct a b) (FreeProduct a b)
 
 instance (Show a, Show b) => Show (FreeProduct a b) where
         show (FreeProd a b) = (show a) ++ (show b)
         show (FreePlus a p) = (show a) ++ "⊕" ++ (show p)
+
+instance (Repr a, Repr b) => Repr (FreeProduct a b) where
+        repr (FreeProd a b) = (repr a) ++ (repr b)
+        repr (FreePlus a p) = (repr a) ++ "plus" ++ (repr p)
+
 
 instance (Poset a, Poset b) => Eq (FreeProduct a b) where
         a == b = a .<. b && b .<. a
@@ -40,11 +48,15 @@ freeToList (FreePlus a b) = a:(freeToList b)
 freeFromList :: (Logic a, Logic b) => [FreeProduct a b] -> FreeProduct a b
 freeFromList alist = foldl1 (<+>) alist
 
-data BoxProduct a b = BoxProduct [FreeProduct a b] --deriving (Show)
+data BoxProduct a b = BoxProduct [FreeProduct a b]
 
 instance (Show a, Show b) => Show (BoxProduct a b) where
         show (BoxProduct []) = "Not exists"
         show (BoxProduct a) = "⟨" ++ (show $ head a) ++ "⟩"
+
+instance (Repr a, Repr b) => Repr (BoxProduct a b) where
+        repr (BoxProduct []) = "Not exists"
+        repr (BoxProduct a) = repr (head a)
 
 instance (Logic a, Logic b) => Eq (BoxProduct a b) where
         (BoxProduct []) == (BoxProduct []) = True
@@ -54,12 +66,16 @@ instance (Logic a, Logic b) => Poset (BoxProduct a b) where
         (BoxProduct as) .<. (BoxProduct bs) = any id [a .<. b | a <- as, b <- bs]
 
 instance (AtomicLogic a, AtomicLogic b) => Finite (BoxProduct a b) where
-        elements = zerozero:(oneone:others)
+        elements = reduceBoxProductList $ zerozero:(oneone:others)
             where
                 zerozero = fromFreeProduct $ zero <> zero
                 oneone = fromFreeProduct $ one <> one
+                -- others = fixedPointBy (eqLen) (accumulateAndExtend) bppairs 
+                -- accumulateAndExtend a = reduceBoxProductList $ a ++ (extendByList pairs a)
+                -- eqLen a b = length a == length b
                 others = concat $ takeWhile ((> 1) . length) $ iterate (extendByList pairs) atoms 
                 pairs = [a <> b | a <- atoms, b <- atoms]
+                bppairs = map fromFreeProduct pairs
 
 instance (AtomicLogic a, AtomicLogic b) => Logic (BoxProduct a b) where
         one = fromFreeProduct $ one <> one
@@ -131,7 +147,7 @@ reduceAll a
 
         eqLen a b = length a == length b
 
-reduceLeftRight' :: (Ord a, Ord b, Logic a, Logic b) => [FreeProduct a b] -> Maybe [FreeProduct a b]
+reduceLeftRight' :: (Logic a, Logic b) => [FreeProduct a b] -> Maybe [FreeProduct a b]
 reduceLeftRight' a = (Just $ reduceSubOrdered a) >>= reduceLeft' >>= reduceRight'
 
 reduceSubOrdered :: (Logic a, Logic b) => [FreeProduct a b] -> [FreeProduct a b]
@@ -217,200 +233,6 @@ extend a@(FreeProd _ _) boxes =
         notLessThan a c = not $ lessThan a c
         lessThan a (BoxProduct c) = any (a .<.) c
 
--- Test cases
---
-freex0one = (X0 <> X0) <+> (X0 <> X1)
-x0one = boxFromList [(X0 <> One), (X0 <> X0) <+> (X0 <> X1), (X0 <> Y0) <+> (X0 <> Y1)]
-x0x0 = boxFromList [(X0 <> X0)]
-
-l4l4elements = elements :: [BoxProduct Lattice4 Lattice4]
-l4l4a = atoms :: [BoxProduct Lattice4 Lattice4]
-l4l4fpairs = [a <> b | a <- (atoms :: [Lattice4]), b <- (atoms :: [Lattice4])]
--- -- a = boxFromList [(X0 <> X1) <+> (X0 <> X0)]
--- -- b = boxFromList [(X1 <> X1) <+> (X1 <> X0)]
--- 
--- -- exprEq a@(BoxProd a1 a2) b@(BoxProd b1 b2) = a1 == b1 && a2 == b2
--- -- exprEq (BoxPlus a b) (BoxPlus c d) = a == c && b == d
--- -- exprEq _ _ = False
--- -- 
--- -- atomsLessThan :: (Atomic a, Logic a, Poset a, Atomic b, Logic b, Poset b) => BoxProduct a b -> [BoxProduct a b]
--- -- atomsLessThan a = filter (.<. a) atoms
--- 
--- -- instance (Atomic a, Logic a, Logic b, Atomic b) => Atomic (BoxProduct a b) where
--- --         atoms = [a <> b | a <- atoms, b <- atoms]
--- -- 
--- -- -- instance (Atomic a, Logic a, Atomic b, Logic b) => Finite (BoxProduct a b) where
--- -- --         elements = [zero <> zero, one <> one] ++ atoms ++ 
--- -- 
--- -- 
--- -- addAtoms :: (Atomic a, Logic a, Atomic b, Logic b) => [BoxProduct a b] -> [BoxProduct a b]
--- -- addAtoms [] = atoms
--- -- addAtoms as = nub $ catMaybes $ [reduceAll $ a <+> p | a <- atoms, p <- as, (not $ a .<. p)]
--- -- 
--- -- isAtom a = any (exprEq a) atoms
--- 
--- -- instance (Logic a, Logic b) => Poset (BoxProduct a b) where
--- --         (.<.) (BoxProd a1 a2) (BoxProd b1 b2) = a1 .<. b1 && a2 .<. b2 
--- --         -- (.<.) a@(BoxProd a1 a2) b@(BoxProd b1 b2)
--- --         --     | isAtom a = (a1 .<. b1) && (a2 .<. b2)
--- --         --     | otherwise = (atomsLessThan a) `subsetOf` (atomsLessThan b)
--- --         -- (.<.) a@(BoxProd _ _) (BoxPlus b c) = (a .<. b) || (a .<. c)
--- --         -- (.<.) (BoxPlus a b) c = (a .<. c) && (b .<. c)
--- -- 
--- -- 
--- -- expandBy :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> BoxProduct a b
--- -- expandBy a@(BoxProd a1 a2) b@(BoxProd b1 b2) 
--- --     | b .<. a = b <+> b1cb2 <+> b1b2c <+> b1cb2c
--- --     | otherwise = a
--- --     where
--- --         b1cb2 = (a1 <-> b1) <> b2
--- --         b1b2c = b1 <> (a2 <-> b2)
--- --         b1cb2c = (a1 <-> b1) <> (a2 <-> b2)
--- -- -- expandBy (BoxPlus a b) c@(BoxProd _ _) = (a `boxExpandBy` c) (b `boxExpandBy` c)
--- -- expandBy a@(BoxProd _ _) (BoxPlus b c) = (a `expandBy` b) `expandBy` c
--- -- -- expandBy p@(BoxPlus _ _) (BoxPlus a b) = (p `boxExpandBy` a) `boxExpandBy` b
--- -- 
--- -- subsetOf :: (Eq a) => [a] -> [a] -> Bool
--- -- a `subsetOf` b = all (`elem` b) a
--- -- 
--- -- reduceAllList :: (Atomic a, Logic a, Atomic b, Logic b) => BoxProduct a b -> [Maybe (BoxProduct a b)]
--- -- reduceAllList a = fixedPointListBy (lifted exprEq) (reduce =<<) $ Just a
--- --     where
--- --         lifted f (Just a) (Just b) = f a b
--- --         lifted _ Nothing Nothing = True
--- --         lifted _ _ _ = False
--- -- 
--- -- -- plus :: (Logic a, Logic b) => BoxProduct a b -> Equivalent (BoxProduct a b) -> Equivalent (BoxProduct a b)
--- -- -- plus a@(BoxProd _ _) b@(Equivalent bs) = foldl1 mergeEquivalenceClass $ map (reduceList . (a <+>)) $ bs
--- -- -- 
--- -- -- reduceList :: (Logic a, Logic b) => BoxProduct a b -> Equivalent (BoxProduct a b)
--- -- -- reduceList a@(BoxProd _ _) = Equivalent [a]
--- -- reduceList p@(BoxPlus a@(BoxProd _ _) b@(BoxProd _ _))
--- --     | c == Nothing = Equivalent []
--- --     | otherwise = Equivalent $ nub $ [p, (fromJust c)]
--- --     where
--- --         c = reduce p
--- -- reduceList p@(BoxPlus a@(BoxProd _ _) (BoxPlus b c)) 
--- --     | reduce_head == Nothing = Equivalent []
--- --     | otherwise = extendEquivalenceClass reduce_rest $ fromJust reduce_head
--- --     where
--- --         reduce_head = liftM (<+> c) $ reduce $ a <+> b
--- --         reduce_rest = prependPlus b $ reduceList $ a <+> c
--- -- 
--- -- prependPlus :: (Logic a, Logic b) => BoxProduct a b -> Equivalent (BoxProduct a b) -> Equivalent (BoxProduct a b)
--- -- prependPlus a@(BoxProd _ _) (Equivalent list) = Equivalent $ map (a <+>) list 
--- 
--- -- reduceList :: (Logic a, Logic b) => BoxProduct a b -> Equivalent (BoxProduct a b)
--- -- reduceList a@(BoxProd _ _) = Equivalent [a]
--- -- reduceList (BoxPlus a b) = reduceList' a b
--- 
--- ---
--- -- boxLess :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> Bool
--- -- boxLess a b = boxLess' (a `boxExpandBy` b) (b `boxExpandBy` a)
--- -- 
--- -- boxLess' (BoxProd a1 a2) (BoxProd b1 b2) = (a1 .<. b1) && (a2 .<. b2)
--- -- boxLess' a@(BoxProd _ _) (BoxPlus b c) = (a `boxLess'` b) || (a `boxLess'` c)
--- -- boxLess' (BoxPlus a b) c@(BoxProd _ _) = (a `boxLess'` c) && (b `boxLess'` c)
--- -- boxLess' p@(BoxPlus _ _) q = all (`boxLess'` q) $ boxPlusToList  p --`boxExpandBy` q
--- -- 
--- -- boxExpandBy :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> BoxProduct a b
--- -- boxExpandBy a@(BoxProd a1 a2) b@(BoxProd b1 b2) 
--- --     | b `boxLess'` a = boxNormalForm $ BoxPlus b $ BoxPlus b1cb2 $ BoxPlus b1b2c b1cb2c
--- --     | otherwise = a
--- --     where
--- --         b1cb2 = (a1 <-> b1) <> b2
--- --         b1b2c = b1 <> (a2 <-> b2)
--- --         b1cb2c = (a1 <-> b1) <> (a2 <-> b2)
--- -- boxExpandBy (BoxPlus a b) c@(BoxProd _ _) = boxNormalForm $ BoxPlus (a `boxExpandBy` c) (b `boxExpandBy` c)
--- -- boxExpandBy a@(BoxProd _ _) (BoxPlus b c) = boxNormalForm $ (a `boxExpandBy` b) `boxExpandBy` c
--- -- boxExpandBy p@(BoxPlus _ _) (BoxPlus a b) = (p `boxExpandBy` a) `boxExpandBy` b
--- -- 
--- -- boxPlus :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> BoxProduct a b
--- -- boxPlus a@(BoxProd a1 a2) b@(BoxProd b1 b2)
--- --     | a `boxLess` b = b
--- --     | b `boxLess` a = a
--- --     | a1 == b1 = if a2 `isOrthogonal` b2 then a1 <> (a2 \./ b2) else one <> one
--- --     | a2 == b2 = if a1 `isOrthogonal` b1 then (a1 \./ b1) <> a2 else one <> one
--- --     | (a1 /= b1) && (a2 `isOrthogonal` b2) = BoxPlus a b
--- --     | (a2 /= b2) && (a1 `isOrthogonal` b1) = BoxPlus a b
--- --     | otherwise = BoxProd one one
--- -- boxPlus a@(BoxProd _ _) p@(BoxPlus b rest) = boxPlusReduce (boxPlus a b) rest
--- -- boxPlus p@(BoxPlus _ _) a@(BoxProd _ _) = boxPlus a p
--- -- boxPlus (BoxPlus a b) p@(BoxPlus _ _) = boxPlus (boxPlus a p) b
--- -- 
--- -- boxPlusReduce a@(BoxProd _ _) p = boxPlus a p
--- -- boxPlusReduce (BoxPlus a as) p = boxPlus as $ boxPlus a p
--- -- 
--- -- safeBoxPlus :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> Maybe (BoxProduct a b)
--- -- safeBoxPlus a@(BoxProd a1 a2) b@(BoxProd b1 b2)
--- --     | a `boxLess` b = Just b
--- --     | b `boxLess` a = Just a
--- --     | a1 == b1 = if a2 `isOrthogonal` b2 then Just (a1 <> (a2 \./ b2)) else Nothing
--- --     | a2 == b2 = if a1 `isOrthogonal` b1 then Just ((a1 \./ b1) <> a2) else Nothing
--- --     | (a1 /= b1) && (a2 `isOrthogonal` b2) = Just $ BoxPlus a b
--- --     | (a2 /= b2) && (a1 `isOrthogonal` b1) = Just $ BoxPlus a b
--- --     | otherwise = Nothing
--- -- safeBoxPlus a@(BoxProd _ _) p@(BoxPlus b rest) = (safeBoxPlus a b) >>= (`safeBoxPlusReduce` rest)
--- -- safeBoxPlus p@(BoxPlus _ _) a@(BoxProd _ _) = safeBoxPlus a p
--- -- safeBoxPlus (BoxPlus a b) p@(BoxPlus _ _) = (safeBoxPlus a p) >>= (`safeBoxPlus` b)
--- -- 
--- -- safeBoxPlusReduce :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> Maybe (BoxProduct a b)
--- -- safeBoxPlusReduce a@(BoxProd _ _) p = safeBoxPlus a p
--- -- safeBoxPlusReduce (BoxPlus a as) p = (safeBoxPlus as) =<< (safeBoxPlus a p)
--- -- 
--- -- (<+>) :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> BoxProduct a b
--- -- (<+>) = boxPlus
--- -- 
--- -- boxNormalForm :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b
--- -- boxNormalForm p 
--- --     | length terms > 0 = foldl1 (BoxPlus) terms
--- --     | otherwise = BoxProd zero zero
--- --     where
--- --         terms = filter dropZero $ boxPlusToList p
--- --         dropZero (BoxProd a b)
--- --             | a == zero && b == zero = False
--- --             | otherwise = True
--- -- 
--- -- boxReduce :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b
--- -- boxReduce p = foldl1 (boxPlus) $ terms
--- --     where
--- --         terms = nub $ filter (/= z) $ boxPlusToList p
--- --         z = BoxProd zero zero
--- -- 
--- -- boxPlusToList :: (Logic a, Logic b) => BoxProduct a b -> [BoxProduct a b]
--- -- boxPlusToList a@(BoxProd _ _) = [a]
--- -- boxPlusToList (BoxPlus a b) = boxPlusToList a ++ boxPlusToList b
--- -- 
--- -- instance (Logic a, Logic b) => Eq (BoxProduct a b) where
--- --         (==) a b = (a `boxLess` b) && (b `boxLess` a)
--- -- 
--- -- allBoxProducts :: (Logic a, Logic b) => [BoxProduct a b]
--- -- allBoxProducts = [boxProd a b | a <- elements, b <- elements]
--- -- 
--- -- allBoxSums :: (Logic a, Logic b) => [BoxProduct a b] -> [BoxProduct a b]
--- -- allBoxSums [] = allBoxProducts
--- -- allBoxSums as = nub $ [a `boxPlus` b | a <- as, b <- allBoxProducts]
--- -- 
--- -- instance (Logic a, Logic b) => Finite (BoxProduct a b) where
--- --         elements = stable $ iterate allBoxSums allBoxProducts  
--- --             where
--- --                 stable (x:ys@(y:_))
--- --                     | length x == length y = y
--- --                     | otherwise = stable ys
--- -- 
--- -- instance (Logic a, Logic b) => Poset (BoxProduct a b) where
--- --         (.<.) = boxLess
--- -- 
--- -- instance (Logic a, Logic b) => Logic (BoxProduct a b) where
--- --         one = BoxProd one one
--- --         zero = BoxProd zero zero
--- --         ortho q = head $ filter (\p -> (isBoxOrthogonal q p) && (p `safeBoxPlus` q == Just one)) elements
--- -- 
--- -- isBoxOrthogonal :: (Logic a, Logic b) => BoxProduct a b -> BoxProduct a b -> Bool
--- -- isBoxOrthogonal (BoxProd a1 a2) (BoxProd b1 b2) = (a1 `isOrthogonal` b1) || (a2 `isOrthogonal` b2)
--- -- isBoxOrthogonal a@(BoxProd _ _) (BoxPlus c d) = (a `isBoxOrthogonal` c) && (a `isBoxOrthogonal` d)
--- -- isBoxOrthogonal p@(BoxPlus _ _) a@(BoxProd _ _) = a `isBoxOrthogonal` p
--- -- isBoxOrthogonal (BoxPlus a b) p@(BoxPlus _ _) = (a `isBoxOrthogonal` p) && (b `isBoxOrthogonal` p)
 
 (<->) :: (Logic a) => a -> a -> a
 b <-> a = fromJust $ b /\ ortho a
@@ -442,4 +264,22 @@ fixedPointBy eq f x0 = stable $ iterate f x0
 
 fixedPoint :: (Eq a) => (a -> a) -> a -> a
 fixedPoint = fixedPointBy (==) 
+
+createStaticBoxProduct :: (Repr a, Repr b, AtomicLogic a, AtomicLogic b) => String -> [BoxProduct a b] -> [BoxProduct a b] -> String
+createStaticBoxProduct name at el = unlines $ ["import QLogic", dataline, finiteinstance, posetinstance, logicinstance, atomicinstance]
+        where
+            dataline = "data TwoTwoBoxWorld = " ++ (intercalate " | " $ map repr el) ++ " deriving (Enum, Bounded, Eq, Show)"
+            finiteinstance = "instance Finite TwoTwoBoxWorld where\n"
+                                ++ indent ++ "elements = [minBound..]\n" 
+            posetinstance = "instance Poset TwoTwoBoxWorld where\n" ++ (unlines $ map posetRelation el) ++ indent ++ "_ .<. _ = False\n"
+            logicinstance = "instance Logic TwoTwoBoxWorld where\n"
+                                ++ indent ++ "zero = " ++ (repr $ el !! 0) ++ "\n"
+                                ++ indent ++ "one = " ++ (repr $ el !! 1) ++ "\n"
+                                ++ (unlines $ map orthoOf el) ++ "\n"
+            atomicinstance = "instance AtomicLogic TwoTwoBoxWorld where\n"
+                                ++ indent ++ "atoms = [" ++ (intercalate ", " $ map repr $ at) ++ "]\n"
+
+            posetRelation a = unlines $ map (\b -> indent ++ (repr a) ++ " .<. " ++ (repr b) ++ " = True") $ filter (a .<.) el
+            orthoOf a = indent ++ "ortho " ++ (repr a) ++ " = " ++ (repr $ ortho a)
+            indent = "    "
 
