f17cafab26dac6293b91623b8e509997c9f10469
Tomasz Tylec <ttylec@gmail.com>
2014-10-21 10:12:32 +0200

    Refactoring files: QLogic.Tools module

--- QLogic/BoxProduct.hs
+++ QLogic/BoxProduct.hs
@@ -1,6 +1,7 @@
 module QLogic.BoxProduct where
 
 import QLogic
+import QLogic.Tools
 import Data.List
 import Data.Maybe
 import Data.Function
@@ -233,34 +234,6 @@ extend a@(FreeProd _ _) boxes =
 
 (<->) :: (Logic a) => a -> a -> a
 b <-> a = fromJust $ b /\ ortho a
-
-listCycleBy :: (a -> a -> Bool) -> [a] -> a
-listCycleBy _ (x:[]) = x
-listCycleBy eq xs =  firstCycle' [] xs
-    where
-        firstCycle' prefix (x:xs)
-            | any (eq x) prefix = x
-            | otherwise = firstCycle' (x:prefix) xs
-
-firstCycleBy :: (a -> a -> Bool) -> (a -> a) -> a -> a
-firstCycleBy eq f x0 = listCycleBy eq $ iterate f x0
-
-fixedPointListBy :: (a -> a -> Bool) -> (a -> a) -> a -> [a]
-fixedPointListBy eq f x0 = takeUntilStable $ iterate f x0
-    where
-        takeUntilStable (x:ys@(y:_))
-            | x `eq` y = [y]
-            | otherwise = x:(takeUntilStable ys)
-
-fixedPointBy :: (a -> a -> Bool) -> (a -> a) -> a -> a 
-fixedPointBy eq f x0 = stable $ iterate f x0
-    where
-        stable (x:ys@(y:_))
-            | x `eq` y = y
-            | otherwise = stable ys
-
-fixedPoint :: (Eq a) => (a -> a) -> a -> a
-fixedPoint = fixedPointBy (==) 
 
 createStaticBoxProduct :: (Repr a, Repr b, AtomicLogic a, AtomicLogic b) => String -> [BoxProduct a b] -> [BoxProduct a b] -> String
 createStaticBoxProduct name at el = unlines $ ["import QLogic", dataline, finiteinstance, posetinstance, logicinstance, atomicinstance]
