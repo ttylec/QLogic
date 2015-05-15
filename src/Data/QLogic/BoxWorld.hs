c6cec225d445290ff7f373e7d62818f94c004be0
Tomasz Tylec <ttylec@gmail.com>
2015-05-15 10:57:05 +0200

    more abstract implementation of BoxWorld concrete construction

--- src/Data/QLogic/BoxWorld.hs
+++ src/Data/QLogic/BoxWorld.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE GADTs #-} 
+{-# LANGUAGE GADTs, DeriveFunctor, DeriveFoldable #-} 
 module Data.QLogic.BoxWorld where
 
-import Data.List
+import Prelude hiding (concat, foldl1, find, foldl, and)
+import Data.List (intercalate)
 import Data.Maybe
 
 import Data.Set (Set)
@@ -20,10 +21,19 @@ import Data.ByteString.Char8 (pack, unpack)
 
 import Data.QLogic.Utils
 
+import Control.Applicative
+import Data.Foldable
+import Data.Monoid
+
 data Observable = Observable { name   :: Char
                              , domain :: [Int] } deriving (Eq, Ord, Show)
 
-type OneSystem = Map Char Int
+newtype OneSystem = OneSystem (Map Char Int) deriving (Eq, Ord)
+
+instance Show OneSystem where
+    show (OneSystem p) = "(" ++ (intercalate "," . map fmt . Map.toList $ p) ++ ")"
+        where
+            fmt (k, x) = k:'=':show x
 
 newtype PhaseSpace a = PhaseSpace (Set a) deriving (Show)
 
@@ -37,7 +47,7 @@ phaseSpace :: (Composite a, Ord (a OneSystem)) => a [Observable] -> PhaseSpace (
 phaseSpace obs = PhaseSpace $ Set.fromList . compose . fmap phaseSpace1' $ obs
 
 phaseSpace1' :: [Observable] -> [OneSystem]
-phaseSpace1' obs = map Map.fromList $ tuples [[(name o, k) | k <- domain o] | o <- obs]
+phaseSpace1' obs = map (OneSystem . Map.fromList) $ tuples [[(name o, k) | k <- domain o] | o <- obs]
     where
         tuples (v:[]) = map (:[]) v
         tuples (v:vs) = [p:ps | p <- v, ps <- tuples vs]
@@ -55,8 +65,9 @@ instance (Show a) => Show (Question a) where
 infixl 4 <+>
 (<+>) = OPlus
 
-newtype One a = One a deriving (Eq, Ord)
-newtype Two a = Two (a, a) deriving (Eq, Ord)
+newtype One a = One a deriving (Eq, Foldable, Functor, Ord)
+newtype Two a = Two (a, a) deriving (Eq, Foldable, Functor, Ord)
+newtype Three a = Three (a, a, a) deriving (Eq, Ord)  --Foldable, Functor, Ord)
 
 instance (Show a) => Show (One a) where
     show (One a) = "[" ++ show a ++ "]"
@@ -64,43 +75,94 @@ instance (Show a) => Show (One a) where
 instance (Show a) => Show (Two a) where
     show (Two (a, b)) = "[" ++ show a ++ show b ++ "]"
 
-class (Functor a) => Composite a where
-    -- This can be generalized to applicative functor,
-    -- but I need to learn a about it (esp. how to write
-    -- instance for Composite Two.
+instance (Show a) => Show (Three a) where
+    show (Three (a, b, c)) = "[" ++ show a ++ show b ++ show c ++ "]"
+
+instance Functor Three where
+    f `fmap` (Three (a, b, c)) = Three (f a, f b, f c)
+
+instance Foldable Three where
+    f `foldMap` (Three (a, b, c)) = f a `mappend` f b `mappend` f c
+
+class (Foldable a, Applicative a) => Composite a where
     compose     :: a [b] -> [a b]
-    -- Similarly, askAtomic and askQuestion should be
-    -- expressible by some good combinators of Composite class
-    askAtomic   :: a AtomicQuestion -> a OneSystem -> Bool
-    askQuestion :: Question (a AtomicQuestion) -> a OneSystem -> Bool
-    askQuestion (Primitive q) x = askAtomic q x
-    askQuestion (OPlus p q) x = askQuestion p x || askQuestion q x
-    -- In fact Composite class is probably some Monad, MonadPlus
-    -- or sth like that. I have to read about these abstractions
-    -- and implement it here.
-
-instance Functor One where
-    f `fmap` (One a) = One $ f a
-
-instance Composite One where
-    compose (One qs) = map One qs
-    askAtomic (One (AtomicQuestion a alpha)) (One p) = case Map.lookup a p of
+    -- The latter should be more general than compose,
+    -- working for any Applicative functor.
+    -- Question: is it any standard operation for two applicatives?
+    -- It seems that yes, then we wouldn't need this class at all.
+    composeA :: (Applicative f) => a (f b) -> f (a b)
+    
+askAtomicOne :: AtomicQuestion -> OneSystem -> Bool
+askAtomicOne (AtomicQuestion a alpha) (OneSystem p) = case Map.lookup a p of
         Just x  -> x == alpha
         Nothing -> False
 
-instance Functor Two where
-    f `fmap` (Two (a, b)) = Two (f a, f b)
+askQA :: (Foldable a, Applicative a) => Question (a AtomicQuestion) -> a OneSystem -> Bool
+askQA (Primitive a) p = and $ liftA2 askAtomicOne a p 
+askQA (OPlus a b) p = askQA a p || askQA b p 
+
+instance Applicative One where
+    pure a = One a
+    (One f) <*> (One a) = One $ f a
+
+-- Check laws:
+-- identity:
+-- > pure id <*> One v = One id <*> One v = One $ id v = One v -- OK
+-- composition:
+-- > pure (.) <*> One u <*> One v <*> One w = One (.) <*> One u <*> One v <*> One w
+-- > = One ( (.) u ) <*> One v <*> One w = One ( u . v ) <*> One w = One (u . v $ w)
+-- on the other hand:
+-- > One u <*> (One v <*> One w) = One u <*> One (v w) = One $ u (v w) -- OK
+-- homomorphism
+-- > pure f <*> pure x = One f <*> One x = One $ f x = pure $ f x -- OK
+-- interchange:
+-- > One u <*> pure y = One $ u y
+-- > pure ($ y) <*> One u = One ($ y) <*> One u = One (($ y) u) = One (u $ y) -- OK
+
+instance Applicative Two where
+    pure a = Two (a, a)
+    (Two (f, g)) <*> (Two (a, b)) = Two (f a, g b)
+
+-- Check laws:
+-- identity:
+-- > pure id <*> Two (v, w) = Two (id, id) <*> Two (v, w) = Two $ (id v, id w) = Two (v, w) -- OK
+-- composition:
+-- > pure (.) <*> Two (u, u') <*> Two (v, v') <*> Two (w, w') 
+-- > = Two ((.), (.)) <*> Two (u, u') <*> Two (v, v') <*> Two (w, w')
+-- > = Two ( (.) u, (.) u' ) <*>  Two (v, v') <*> Two (w, w') 
+-- > = Two ( u . v, u . v' ) <*> Two (w, w') = Two (u . v $ w, u' . v' $ w')
+-- on the other hand:
+-- > Two (u, u') <*> (Two (v, v') <*> Two (w, w')) 
+-- > = Two (u, u') <*> Two (v w, v' w) = Two (u (v w), u' (v' w')) -- OK
+-- homomorphism
+-- > pure f <*> pure x = Two (f, f) <*> Two (x, x) = Two (f x, f x) = pure $ f x -- OK
+-- interchange:
+-- > Two (u, v) <*> pure y = Two (u y, v y)
+-- > pure ($ y) <*> Two (u, v) = Two ($ y, $ y) <*> Two (u, v) 
+-- > = Two (($ y) u, ($ y) v) = Two (u $ y, v $ y) -- OK
+
+instance Applicative Three where
+    pure a = Three (a, a, a)
+    (Three (f, g, h)) <*> (Three (a, b, c)) = Three (f a, g b, h c)
+
+instance Composite One where
+    compose (One qs) = map One qs
+    composeA (One a) = fmap One a
 
 instance Composite Two where
     compose (Two (ps, qs)) = [Two (p, q) | p <- ps, q <- qs]
-    askAtomic (Two (p, q)) (Two (x, y)) = askAtomic (One p) (One x) 
-                                       && askAtomic (One q) (One y) 
+    composeA (Two (ps, qs)) = curry Two <$> ps <*> qs
+
+instance Composite Three where
+    compose (Three (ps, qs, rs)) = [Three (p, q, r) | p <- ps, q <- qs, r <- rs]
+    composeA (Three (ps, qs, rs)) = (\p q r -> Three (p, q, r)) <$> ps <*> qs <*> rs
 
 atomicQuestionsOf :: Observable -> [AtomicQuestion]
 atomicQuestionsOf obs = map (AtomicQuestion $ name obs) $ domain obs
 
 phaseSubset :: (Composite a) => PhaseSpace (a OneSystem) -> Question (a AtomicQuestion) -> Set (a OneSystem)
-phaseSubset (PhaseSpace points) q = Set.filter (askQuestion q) points
+phaseSubset (PhaseSpace points) q = Set.filter (askQA q) points
+-- phaseSubset (PhaseSpace points) q = Set.filter (askQuestion q) points
 
 boxWorldAtomicQs :: (Composite a) => a [Observable] -> [Question (a AtomicQuestion)]
 boxWorldAtomicQs obs = map Primitive . compose . fmap atomsOf $ obs
@@ -155,3 +217,25 @@ parseQ2Atom = do
     beta <- decimal
     char ']'
     return . Primitive $ Two (AtomicQuestion a alpha, AtomicQuestion b beta) 
+
+readQ3 :: String -> Question (Three AtomicQuestion)
+readQ3 str = case parseOnly parseQ3 $ pack str of 
+    Right q  -> q
+    Left err -> error $ "Error parsing: " ++ err
+
+parseQ3 :: Parser (Question (Three AtomicQuestion))
+parseQ3 = do
+    (q:qs) <- parseQ3Atom `sepBy` (char '+')
+    return $ foldl' (<+>) q qs
+
+parseQ3Atom :: Parser (Question (Three AtomicQuestion))
+parseQ3Atom = do
+    char '['
+    a <- letter_ascii
+    alpha <- decimal
+    b <- letter_ascii
+    beta <- decimal
+    c <- letter_ascii
+    gamma <- decimal
+    char ']'
+    return . Primitive $ Three (AtomicQuestion a alpha, AtomicQuestion b beta, AtomicQuestion c gamma) 
---
+++ src/concrete3.hs
@@ -0,0 +1,33 @@
+import Data.QLogic.BoxWorld
+import Data.QLogic
+
+import Data.List
+import Data.Poset.ConcretePoset
+import Data.QLogic
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IS
+import Data.QLogic.Utils
+import Data.Maybe
+
+x = Observable 'X' [0, 1]
+y = Observable 'Y' [0, 1]
+
+box = [x, y]
+system = Three (box, box, box)
+-- system = Two (box, box)
+-- system = Two ((Two (box, box)), box)
+
+main :: IO ()
+main = do
+    let (q2set, set2q, ql) = boxWorldLogic system
+        a = readQ3 "[X0X0X0]+[X1X0X0]+[X0X1Y0]+[X1X1Y0]"
+        b = readQ3 "[Y0X0X0]+[Y0X0X1]+[Y0X1X0]+[Y0X1X1]"
+
+    print $ q2set a
+    print $ q2set b
+    print $ set2q . q2set $ b
+    print $ sum . map (length . set2q) $ elementsOf ql
+    print $ compatIn ql (q2set a) (q2set b) 
+    print $ length . elementsOf $ ql
