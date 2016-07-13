14d3881196d1a36d32d64395e8f6abdb000cb32f
Tomasz Tylec <ttylec@gmail.com>
2016-07-13 07:52:49 +0200

    Research stuff

--- src/3boxl.hs
+++ src/3boxl.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
 module Main (main) where
 
 import System.Environment
@@ -80,7 +79,7 @@ generateOMP ql atoms accum
     ortho = orthoIn ql
 
 generateEA :: (QLogicStruct p a, Ord a) => p -> [a] -> Set a -> Set a
-generateEA ql !atoms !accum
+generateEA ql atoms accum
   | Set.size accum == Set.size next = accum
   | otherwise = generateEA ql atoms next
   where
@@ -88,10 +87,10 @@ generateEA ql !atoms !accum
     complements = Set.map (ocmplIn ql) accum
     sums = Set.fromList . sums' . Set.toList $ accum
     sums' [] = []
-    sums' !(a:as) = go as ++ sums' as
+    sums' (a:as) = go as ++ sums' as
       where
-        go !ls = filter (not . null . decomposeInto ql atoms . ocmplIn ql)
-                 . map (a \/) . filter (`ortho` a) $ ls
+        go = filter (not . null . decomposeInto ql atoms . ocmplIn ql)
+             . map (a \/) . filter (`ortho` a)
     (\/) = unsafeSupIn ql
     ortho = orthoIn ql
 
@@ -115,14 +114,88 @@ boxWorldPropositions' obs typ = typ (booleanAlgebra ps) (map q2set atomicQs)
     atomicQs  = boxAtoms obs
     q2set     = phaseSubset phase
 
+-- boxWorldALogic :: (System s, Ord (s Point), Ord (s Box))
+--               => BoxModel s
+--               -> Representation ConcreteInt IntSet (Question (s Box))
+boxWorldALogic obs = Representation q2set set2q ql
+  where
+    ql        = concreteIntSublogic' (booleanAlgebraInt space) (map q2set atomicQs)
+    phase     = phaseSpace obs
+    atomicQs  = boxAtoms obs
+    phasePack = packList . phasePoints $ phase
+    packSet   = IS.fromList . map (toKey phasePack) . Set.toList
+    space     = IS.fromList [0..length atomicQs - 1]
+    q2set     = packSet . phaseSubset phase
+    invAtoms  = rightInvMap atomicQs q2set
+    set2q q
+        | IS.null q = nullQ
+        | otherwise = collapse . sequenceA . map (invAtoms Map.!) . decompose ql $ q
+            where
+              collapse (Question as) = Question . concat $ as
+
 main :: IO ()
 main = do
   let
-    boxes = twoboxes
-    ea = boxWorldPropositions boxes concreteIntSubEA
-    omp = boxWorldPropositions boxes concreteIntSubOMP
-    oml = boxWorldPropositions boxes concreteIntSubOML
+      boxWorld = boxWorldLogic threeboxes
+      boxWorldA= boxWorldALogic threeboxes
+      boxWorldRA= logicRepr boxWorldA
+      boxWorldR = logicRepr boxWorld
+      constr = boxConstraints threeboxes
+      threeBoxElems = filter ((<= 1) . maxStateValue constr)
+                      . elementsOf $ boxWorld
 
-  print . length . elementsOf $ ea
-  print . length . elementsOf $ omp
-  print . length . elementsOf $ oml
+  let
+    gyni1 = read "[X0X0X0]" ::  Question (Three Box)
+    gyni2 = read "[X1Y1Y0]" ::  Question (Three Box)
+    gyni3 = read "[Y0X1Y1]" ::  Question (Three Box)
+    gyni4 = read "[Y1Y0X1]" ::  Question (Three Box)
+
+
+  -- print $ summable constr gyni1 gyni2
+  -- print $ summable constr (gyni1 .@. gyni2) gyni3
+  -- print $ summable constr (gyni1 .@. gyni2 .@. gyni3) gyni4
+
+  let
+    gyni123 = gyni1 .@. gyni2 .@. gyni3
+    gyni = gyni1 .@. gyni2 .@. gyni3 .@. gyni4
+
+  print "Elements of logic:"
+  print $ length . elementsOf $ boxWorld
+  print $ length . elementsOf $ boxWorldA
+  print $ length threeBoxElems
+
+  let
+    strange = filter ((>1) . maxStateValue constr) . elementsOf $ boxWorldA
+
+  print strange
+  print $ map (maxStateValue constr) strange
+  print $ map (ocmplIn boxWorldA) strange
+
+  -- print $ decomposeInto boxWorldRA (atomsOf boxWorldRA) . toRepr boxWorldA $ gyni
+  -- print $ decomposeInto boxWorldRA (atomsOf boxWorldRA) .
+  --   ocmplIn boxWorldRA . toRepr boxWorldA $ gyni
+  -- -- print $ isLogic . logicRepr $ boxWorld
+  -- -- print $ length threeBoxElems
+  -- print "Orthogonality of gyni components"
+  -- print $ orthoIn boxWorld gyni1 gyni2
+  -- print $ orthoIn boxWorld (gyni1 .@. gyni2) gyni3
+  -- print $ orthoIn boxWorld (gyni1 .@. gyni2 .@. gyni3) gyni4
+
+  -- print "Whether gyni exists and what is its complement"
+  -- print $ toRepr boxWorld gyni `elem` elementsOf (logicRepr boxWorld)
+  -- print $ fromRepr boxWorld . toRepr boxWorld $ gyni
+  -- print $ fromRepr boxWorld . ocmplIn boxWorldR . toRepr boxWorld $ gyni
+  -- print $ ocmplIn boxWorldR . toRepr boxWorld $ gyni
+
+  -- let gynic = ocmplIn boxWorldR . toRepr boxWorld $ gyni
+  -- print $ filter (\a -> lessIn boxWorldR a gynic) $ atomsOf boxWorldR
+  -- print $ atomsOf boxWorldR
+
+  -- print $ ocmplIn boxWorld gyni `elem` elementsOf boxWorld
+
+
+
+  -- print $ strongDisjoint (toRepr boxWorld gyni123) (toRepr boxWorld gyni4)
+  -- print $ take (read n) . elementsOf $ boxWorld
+  -- -- print $ take (read n) . elementsOf . logicRepr $ boxWorld
+  -- print $ length threeBoxElems
